1266bd21a307edef29f729d25c78da163f313885
nawiwat <phuphapupa84@gmail.com>
2023-04-19 17:20:05 +0700

    .

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.api_home)
+    path('', views.api_home),
+    path('select', views.select),
+    path('download', views.download)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
-import getData
+from getData import Wat 
+import os
 
 def api_home(request, *args, **kwargs):
     body = request.body
@@ -11,3 +12,30 @@ def api_home(request, *args, **kwargs):
         pass
 
     return JsonResponse({"what": "its working huh"})
+
+def select(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    data = json.loads(body)
+    recv = Wat()
+    recv.province = data["Select(number)"]
+    recv.select_province(recv.province)
+    return JsonResponse(recv.data, safe=False)
+
+def download(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    data = json.loads(body)
+    recv = Wat()
+    recv.province = data["Select(number)"]
+    recv.select_province(recv.province)
+    recv.saveCSV()
+    fileName = recv.provinceName
+    filePath = fileName + ".csv"
+    if os.path.exists(filePath):
+        with open(filePath, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/octet-stream")
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(filePath)
+            return response
+    else:
+        return JsonResponse({"error":"404"})
