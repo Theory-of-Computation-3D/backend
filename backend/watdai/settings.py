53cfff70793429d7107edf18607ac0d7ea96787d
Chayanin Liengjindathaworn <maimokemm@hotmail.com>
2023-04-19 21:01:13 +0700

    add allowed cor

--- backend/watdai/settings.py
+++ backend/watdai/settings.py
@@ -59,10 +59,12 @@ CORS_ALLOW_METHODS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
+    'http://127.0.0.1:5173/'
 ]
 
 MIDDLEWARE = [
