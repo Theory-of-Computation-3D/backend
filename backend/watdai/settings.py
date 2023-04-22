45262e0318742ef522591ce5310fc5a69d9d0b2b
Chayanin Liengjindathaworn <69464429+maimoke@users.noreply.github.com>
2023-04-23 02:13:15 +0700

    change allowed host to match deployed address

--- backend/watdai/settings.py
+++ backend/watdai/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-6a%3_$ib+7ws!id6xo_kkz28=^*hvr0$xe!bddfi8ixd*mk&#5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.128.112.4"]
+ALLOWED_HOSTS = ["35.247.174.148"]
 
 
 # Application definition
