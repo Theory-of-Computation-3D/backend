805fbf284e9e71eb78965d65a1ab009c8642c0e8
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2023-04-22 21:49:20 +0700

    Updated

--- backend/watdai/settings.py
+++ backend/watdai/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-6a%3_$ib+7ws!id6xo_kkz28=^*hvr0$xe!bddfi8ixd*mk&#5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [34.101.124.190]
+ALLOWED_HOSTS = ['34.101.124.190',"127.0.0.1"]
 
 
 # Application definition
